dcfeee8e9d16d02758d91c90e0cd0cbe03971565
parrvaz <47025465+parrvaz@users.noreply.github.com>
2024-08-29 14:43:38 +0330

    list item without test

--- app/components/shared/sidebar/listItem.tsx
+++ app/components/shared/sidebar/listItem.tsx
@@ -1,69 +1,51 @@
 import { ChevronDownIcon, HomeIcon } from "@heroicons/react/20/solid";
-import { ErrorMessage, Field } from "formik";
-import { FC, useState } from "react";
+import { FC } from "react";
+import SubListItem from "./subListItem";
 
 interface ListItemProps {
   name: string;
   lable: string;
-  type?: string;
-  listItemClassName?: string;
-  errorClassName?: string;
-  lableClassName?: string;
+  hasSub: boolean;
+  isOpen?: boolean;
+  setOpen?: any;
+  icon?: any;
+  subList: [any];
 }
 
 const ListItem: FC<ListItemProps> = ({
   name,
   lable,
-  type = "text",
-  listItemClassName,
-  errorClassName,
-  lableClassName,
+  hasSub,
+  isOpen = false,
+  setOpen,
+  icon,
+  subList,
 }) => {
-  const [isOpen, setIsOpen] = useState({
-    dashboard: false,
-    users: false,
-    reports: false,
-  });
-
-  const toggleDropdown = (section: string) => {
-    setIsOpen((prev: any) => ({ ...prev, [section]: !prev[section] }));
-  };
   return (
     <>
-      <li className="mb-4">
+      <li id={name} className="mb-4">
         <a
           href="#"
           className="flex justify-between items-center p-2 bg-white hover:bg-green-300 rounded-md transition-colors duration-300"
-          onClick={() => toggleDropdown("dashboard")}
+          onClick={setOpen}
         >
           <span className="flex items-center justify-start w-full">
-            <HomeIcon className="h-6 w-6 ml-2 text-gray-600" />
-            داشبورد
+            {icon}
+            {lable}
           </span>
-          <ChevronDownIcon
-            className={`h-5 w-5 text-gray-600 transition-transform duration-300 ${
-              isOpen.dashboard ? "rotate-180" : ""
-            }`}
-          />
+          {hasSub && (
+            <ChevronDownIcon
+              className={`h-5 w-5 text-gray-600 transition-transform duration-300 ${
+                isOpen ? "rotate-180" : ""
+              }`}
+            />
+          )}
         </a>
-        {isOpen.dashboard && (
+        {isOpen && (
           <ul className="mt-2 pr-4 space-y-2">
-            <li>
-              <a
-                href="#"
-                className="block p-2 text-sm bg-white hover:bg-green-300 rounded-md text-right transition-colors duration-300"
-              >
-                زیرآیتم ۱
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block p-2 text-sm bg-white hover:bg-green-300 rounded-md text-right transition-colors duration-300"
-              >
-                زیرآیتم ۲
-              </a>
-            </li>
+            {subList.map((item) => (
+              <SubListItem name={item.name} lable={item.lable} />
+            ))}
           </ul>
         )}
       </li>
