1b4d484c8aa9bb2a041322863ce82a49f5095754
parrvaz <47025465+parrvaz@users.noreply.github.com>
2024-09-01 10:14:09 +0330

    delete comments

--- app/components/layouts/panelLayout.tsx
+++ app/components/layouts/panelLayout.tsx
@@ -12,7 +12,7 @@ const PanelLayout: FC<PanelLayoutProps> = ({ children, sidebarData }) => {
   return (
     <>
       <div className="flex flex-col h-screen">
-        <Header /> {/* اضافه کردن هدر */}
+        <Header />
         <div className="flex flex-1 overflow-hidden">
           <Sidebar data={sidebarData.data} />
           <div className="flex-1 p-6 bg-gray-50 overflow-y-auto">
@@ -20,11 +20,6 @@ const PanelLayout: FC<PanelLayoutProps> = ({ children, sidebarData }) => {
           </div>
         </div>
       </div>
-      {/* <Header />
-      <div className="flex h-screen">
-        <Sidebar data={sidebarData.data} />
-        <div className="flex-1 p-6 bg-gray-50 overflow-y-auto">{children}</div>
-      </div> */}
     </>
   );
 };
